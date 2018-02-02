2b71b4cb956fdcaeed0665b6c32546a392607ef1
Toxa <antonkviatofficial@gmail.com>
2018-02-02 12:51:10 +0300

    temp commit

--- audioclass.cpp
+++ audioclass.cpp
@@ -4,44 +4,59 @@ AudioClass::AudioClass(QWidget *parent)
     : QWidget(parent)
 {
     inputBuffer.open(QBuffer::ReadWrite);
-    outputBuffer.open(QBuffer::ReadOnly);
+    outputBuffer.open(QBuffer::ReadWrite);
     QAudioFormat format;
-    format.setSampleRate(8000);
+    format.setSampleRate(44100);
     format.setChannelCount(1);
-    format.setSampleSize(8);
+    format.setSampleSize(16);
     format.setCodec("audio/pcm");
     format.setByteOrder(QAudioFormat::LittleEndian);
     format.setSampleType(QAudioFormat::UnSignedInt);
 
     QAudioDeviceInfo info = QAudioDeviceInfo::defaultInputDevice();
-    qDebug() << info.deviceName();
-         if (!info.isFormatSupported(format)) {
-             qWarning() << "Default format not supported, trying to use the nearest.";
-             format = info.nearestFormat(format);
-         }
+     if (!info.isFormatSupported(format)) {
+         qWarning() << "Default format not supported, trying to use the nearest.";
+         format = info.nearestFormat(format);
+     }
     input = new QAudioInput(format, this);
+    format.setSampleRate(8000);
     output = new QAudioOutput(format, this);
-    input->reset();
-    input->start(&inputBuffer);
-    input->resume();
-    output->start(&outputBuffer);
-    connect(&inputBuffer, &QBuffer::readyRead, [=](){
-        outputBuffer.buffer().resize(inputBuffer.buffer().size());
-        memcpy(outputBuffer.buffer().data(), inputBuffer.buffer().constData(), outputBuffer.buffer().size());
-//        inputBuffer.buffer().remove(0, inputBuffer.buffer().size());
-//        qDebug() << "output size: " << outputBuffer.buffer().size();
-//        qDebug() << "input size: " << inputBuffer.buffer().size();
-
-    });
 
     connect(output, &QAudioOutput::stateChanged, [=](QAudio::State state){
-        qDebug() << input->error();
+        qDebug() << "error output: " << output->error();
         qDebug() <<"output: "  << state;
+
     });
 
     connect(input, &QAudioInput::stateChanged, [=](QAudio::State state){
+        qDebug() << "error input: " << input->error();
         qDebug() <<"input: "  << state;
     });
+    output->start(&outputBuffer);
+    qDebug() << "period size: " << output->periodSize();
+    input->start(&inputBuffer);
+    qDebug() << "buffer size: " << output->bufferSize();
+    QTimer::singleShot(500, Qt::PreciseTimer, [=](){
+    });
+    connect(&inputBuffer, &QBuffer::readyRead, [=](){
+
+        outputBuffer.buffer().resize(inputBuffer.buffer().size());
+
+        memcpy(outputBuffer.buffer().data(), inputBuffer.buffer().constData(), outputBuffer.buffer().size());
+        outputBuffer.seek(0);
+        if (output->state() == QAudio::IdleState){
+            output->stop();
+            output->start(&outputBuffer);
+        }
+        inputBuffer.buffer().clear();
+        inputBuffer.seek(0);
+
+
+//        qDebug() << "------------------";
+//        qDebug() << "input size: " << inputBuffer.size();
+//        qDebug() << "output size: " << outputBuffer.size();
+    });
+
 }
 
 AudioClass::~AudioClass()
